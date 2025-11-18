4c856633ec6db8779f2154ed51c0d661c4291fbb
KeviN-XVII <kevinquartar@gmail.com>
2025-11-18 16:01:24 +0100

    Add Footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import Epibooksnav from './components/epibooksnav.jsx';
 import AllTheBooks from './components/AllTheBooks.jsx';
 import EpiAlert from './components/EpiAlert.jsx';
+import Footer from './components/Footer.jsx';
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
       <Epibooksnav />
       <EpiAlert />
       <AllTheBooks />
+      <Footer />
     </>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+const Footer = function() {
+    return (
+        <>
+            <footer className="footer p-5 bg-dark mt-4">
+                <div className="container text-center">
+                    <span className="text-white">© 2025 EPIBOOKS</span>
+                </div>
+            </footer>
+        </>
+    );
+};
+export default Footer;
