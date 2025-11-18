cdfc13e92e4f7bc1d34557a40dd650c9153a94a5
KeviN-XVII <kevinquartar@gmail.com>
2025-11-18 17:15:41 +0100

    height img

--- src/components/AllTheBooks.jsx
+++ src/components/AllTheBooks.jsx
@@ -13,13 +13,13 @@ function AllTheBooks() {
             return (
                 <Col key={book.asin} md={4} lg={3} sm={6} xs={12} className="mb-4 d-flex justify-content-center">
     <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={book.img} className="w-100 h-100 object-fit-cover"/>
-      <Card.Body>
+      <Card.Img variant="top" src={book.img} className="w-100 object-fit-cover" style={{ height: "450px" }}/>
+      <Card.Body className="d-flex flex-column">
         <Card.Title>{book.title}</Card.Title>
-        <Card.Text>
+        <Card.Text className="flex-grow-1" >
             {book.category} - {book.price}€
         </Card.Text>
-        <Button variant="primary">ACQUISTA</Button>
+        <Button variant="warning">ACQUISTA</Button>
       </Card.Body>
     </Card>
     </Col>
