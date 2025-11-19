9d1603270d8fbc38ce2c5b120dc4647a0045c5fb
KeviN-XVII <kevinquartar@gmail.com>
2025-11-19 15:35:51 +0100

    Extra

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,17 +1,44 @@
 // Crea un componente BookList. Questo componente riceverà dalle prop un array di libri, e li visualizzerà attraverso un .map() ritornando per ogni elemento il componente SingleBook. Successivamente monta BookList dentro il componente App, e forniscigli una lista di libri da uno dei file .json come prop. Dovresti ottenere a schermo un risultato simile al componente AllTheBooks: ora però la struttura è molto più modulare e riutilizzabile.
 import SingleBook from './SingleBook.jsx';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 import { Container, Row} from 'react-bootstrap';
+import { Component } from 'react';
 
-const BookList = (props) => {
+class BookList extends Component {
+    state= {
+        search: ''
+    }
+    render() {
+    const props = this.props;
+    // adesso do come risultato il form di ricerca e sotto la lista dei libri
+    const filteredBooks = props.books.filter((book) =>
+      book.title.toLowerCase().includes(this.state.search.toLowerCase())
+    );
+
     return (
+        <>
         <Container>
+         <Form onSubmit ={(e) => {
+            e.preventDefault();
+            console.log('Cercando il libro:', this.state.search);
+         }}>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Cerca il tuo libro</Form.Label>
+        <Form.Control type="text" placeholder="Cerca il tuo libro" value={this.state.search} onChange={(e) => this.setState({search: e.target.value})}/>
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Cerca
+      </Button>
+    </Form>
             <Row>
-                {props.books.map((book) => (
+                {filteredBooks.map((book) => (
                         <SingleBook book={book} key={book.asin} />
                 ))}
             </Row>
         </Container>
+        </>
     ); 
 }
+}
 export default BookList;
-    
--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -1,23 +1,30 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import {Col} from 'react-bootstrap'
+import { Component } from 'react';
 
-const  SingleBook = (props) => {
-  const { book } = props;
+
+class SingleBook extends Component {
+  state = {
+    selected: false
+  }
+  render() {
+    const { book } = this.props;
     return (
-        <Col key={book.asin} md={4} lg={3} sm={6} xs={12} className="mb-4 d-flex justify-content-center">
-    <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={book.img} className="w-100 object-fit-cover" style={{ height: "450px" }}/>
-      <Card.Body className="d-flex flex-column">
-        <Card.Title>{book.title}</Card.Title>
-        <Card.Text className="flex-grow-1" >
-            {book.category} - {book.price}€
-        </Card.Text>
-        <Button variant="warning">ACQUISTA</Button>
-      </Card.Body>
-    </Card>
+      <Col key={book.asin} md={4} lg={3} sm={6} xs={12} className="mb-4 d-flex justify-content-center">
+    <Card style={{ width: '18rem' }} className={this.state.selected ? 'border border-3 border-warning' : ''} onClick={() => this.setState({ selected: !this.state.selected })}>
+       <Card.Img variant="top" src={book.img} className="w-100 object-fit-cover" style={{ height: "450px" }}/>
+       <Card.Body className="d-flex flex-column">
+         <Card.Title>{book.title}</Card.Title>
+         <Card.Text className="flex-grow-1" >
+             {book.category} - {book.price}€
+         </Card.Text>
+         <Button variant="warning">ACQUISTA</Button>
+       </Card.Body>
+     </Card>
     </Col>
-  );
+    );
+  }
 }
 
 export default SingleBook;
