1989887313e634c6c3900f2e85fd89c0c2799b1e
KeviN-XVII <kevinquartar@gmail.com>
2025-11-24 15:32:37 +0100

    S10-L1 Es

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,41 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
-import Epibooksnav from './components/epibooksnav.jsx';
-import AllTheBooks from './components/AllTheBooks.jsx';
-import EpiAlert from './components/EpiAlert.jsx';
-import Footer from './components/Footer.jsx';
-import SingleBook from './components/SingleBook.jsx';
-import BookList from './components/BookList.jsx';
-import horrorBooks from './horror.json';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Epibooksnav from "./components/epibooksnav.jsx";
+import EpiAlert from "./components/EpiAlert.jsx";
+import Footer from "./components/Footer.jsx";
+import SingleBook from "./components/SingleBook.jsx";
+import BookList from "./components/BookList.jsx";
+import horrorBooks from "./horror.json";
+import CommentArea from "./components/CommentArea.jsx";
+import { Container, Row, Col } from "react-bootstrap";
+import { Component } from "react";
 
-function App() {
+class App extends Component {
+  state = {
+    selectedAsin: null,
+  };
 
-  return (
-    <>
-      <Epibooksnav />
-      <div className="bg-light">
-      <EpiAlert />
-      {/* <SingleBook book={{
-         "asin": "0345546792",
-    "title": "The Silent Corner: A Novel of Suspense (Jane Hawk)",
-    "img": "https://images-na.ssl-images-amazon.com/images/I/91dDIYze1wL.jpg",
-    "price": 7.92,
-    "category": "horror"
-      }} /> */}
-      <BookList books={horrorBooks} />
-      {/* <AllTheBooks /> */}
-      </div>
-      <Footer />
-    </>
-  )
+  onSelectBook = (asin) => {
+    this.setState({ selectedAsin: asin });
+  };
+
+  render() {
+    return (
+      <>
+        <Epibooksnav />
+        <EpiAlert />
+        <div className="d-flex">
+          <BookList
+            books={horrorBooks}
+            onSelectBook={this.onSelectBook}
+            selectedAsin={this.state.selectedAsin}
+          />
+          <CommentArea asinFromApp={this.state.selectedAsin} />
+          {/* <AllTheBooks /> */}
+        </div>
+        <Footer />
+      </>
+    );
+  }
 }
 
-export default App
+export default App;
--- src/components/CommentArea.jsx
+++ src/components/CommentArea.jsx
@@ -1,54 +1,64 @@
 import { Component } from "react";
 import CommentsList from "./CommentsList.jsx";
 import AddComment from "./AddComment.jsx";
+import Spinner from "react-bootstrap/Spinner";
 // CommentArea dovrà fare la fetch delle recensioni per il libro selezionato, e salvare i commenti nel proprio stato. Conterrà inoltre due sotto-componenti: CommentsList and AddComment.
 
 class CommentArea extends Component {
-    state = {
-        comments: [
-            {
-            "author": "",
-            "comment": "",
-            "rate": 1,
-            "elementId": "",
-            "_id": ""
-            }
-        ]
-    };
-    // richiamo la fetch dei commenti del libro selezionato
-    getComments = function () {
+  state = {
+    comments: [
+      {
+        comment: "",
+        rate: 1,
+        elementId: "",
+        _id: "",
+        loading: true,
+      },
+    ],
+  };
+  // richiamo la fetch dei commenti del libro selezionato
+  getComments = function () {
     const URL = "https://striveschool-api.herokuapp.com/api/comments/";
-    fetch(URL + this.props.asin, {
-        headers: {
-            "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2OTFmMDZmZjIzZTc0MDAwMTVmN2ZkYjAiLCJpYXQiOjE3NjM2NDEwODcsImV4cCI6MTc2NDg1MDY4N30.6GoB_3LZYaDREDcXy4hkqItBjCu6cpu2G85rO40sUH0"
-        }
+    fetch(URL + this.props.asinFromApp, {
+      headers: {
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2OTFmMDZmZjIzZTc0MDAwMTVmN2ZkYjAiLCJpYXQiOjE3NjM2NDEwODcsImV4cCI6MTc2NDg1MDY4N30.6GoB_3LZYaDREDcXy4hkqItBjCu6cpu2G85rO40sUH0",
+      },
     })
-    .then((response) => {
+      .then((response) => {
         if (response.ok) {
-            return response.json();
+          return response.json();
         } else {
-            throw new Error("Errore nel recupero dei commenti");
+          throw new Error("Errore nel recupero dei commenti");
         }
-    })
-    .then((arrayComments) => {
+      })
+      .then((arrayComments) => {
         console.log(arrayComments);
-        this.setState({ 
-            comments: arrayComments 
+        this.setState({
+          comments: arrayComments,
+          loading: false,
         });
-    })
-    .catch((err) => console.log("Errore: " + err));
-};
-componentDidMount() {
-    this.getComments();
-}
-
-    render() {
-        return (
-            <div>
-                <CommentsList comments={this.state.comments} />
-                <AddComment asin={this.props.asin} newComments={this.getComments} />
-            </div>
-        );
+      })
+      .catch((err) => console.log("Errore: " + err));
+  };
+  componentDidUpdate(prevProps) {
+    if (prevProps.asinFromApp !== this.props.asinFromApp) {
+      this.getComments();
     }
-};
+  }
+
+  render() {
+    return (
+      <>
+        {this.state.loading && (
+          <Spinner animation="border" variant="warning" className="mt-3" />
+        )}
+        <div>
+          <CommentsList comments={this.state.comments} />
+          {/* <AddComment asin={this.props.asin} newComments={this.getComments} /> */}
+        </div>
+      </>
+    );
+  }
+}
 export default CommentArea;
